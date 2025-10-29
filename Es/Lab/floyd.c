8f6d331a9efb29c93e6d73c6f617dca37539b293
alePrandi <ale.prandi06@gmail.com>
2025-10-29 18:40:51 +0100

    Es floyd

--- Es/Lab/floyd.c
+++ Es/Lab/floyd.c
@@ -5,17 +5,18 @@ int main(){
     int n = 5;
     int numero = 1;
 
-    //int cont = 1;
-    //int max = n*(n/2 + 1);
-    // for(int i = 0; i < max; i++){
-    //     printf("%d ", numero);
-    //     if(numero == cont*(cont + 1)/2){
-    //         printf("\n");
-    //         cont++;
-    //     }
-    //     numero++;
-    // }
-
+    /*    
+    int cont = 1;
+    int max = n*(n/2 + 1);
+     for(int i = 0; i < max; i++){
+         printf("%d ", numero);
+         if(numero == cont*(cont + 1)/2){
+             printf("\n");
+             cont++;
+         }
+         numero++;
+    }
+    */
 
     for(int i=1;i<=n;i++){
         for(int j=0;j<i;j++){
