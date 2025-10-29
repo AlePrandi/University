0081751a3a8b07a04c64e1f241837e68046dc120
alePrandi <ale.prandi06@gmail.com>
2025-10-29 18:38:45 +0100

    Es floyd

--- Es/Lab/floyd.c
+++ Es/Lab/floyd.c
@@ -1,10 +1,24 @@
 #include <stdio.h>
 
 int main(){
+
     int n = 5;
     int numero = 1;
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j <= i; j++){
+
+    //int cont = 1;
+    //int max = n*(n/2 + 1);
+    // for(int i = 0; i < max; i++){
+    //     printf("%d ", numero);
+    //     if(numero == cont*(cont + 1)/2){
+    //         printf("\n");
+    //         cont++;
+    //     }
+    //     numero++;
+    // }
+
+
+    for(int i=1;i<=n;i++){
+        for(int j=0;j<i;j++){
             printf("%d ", numero);
             numero++;
         }
