6910eff621294cb81d3dc334c64ada024fdcdd3e
alePrandi <ale.prandi06@gmail.com>
2025-11-17 15:37:58 +0100

    ricorsione

---
+++ Es/Lezioni/fattoriale.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int fact(int num);
+
+int main(void)
+{
+    int n, val;
+
+    printf("inserisci un numero: ");
+    scanf("%d", &n);
+    val = fact(n);
+    printf("il fattoriale di %d e': %d \n", n, val);
+    return 0;
+}
+
+int fact(int num)
+{
+    if (num >= 0)
+    {
+        if (num == 0)
+        {
+            return 1;
+        }
+        else
+        {
+            return num * fact(num - 1);
+        }
+    }else{
+        printf("numero non valido");
+        return 0;
+    }
+}
