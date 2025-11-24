8ff17223976d5647ae72a2d2b86ac8b45967900f
alePrandi <ale.prandi06@gmail.com>
2025-11-24 14:48:38 +0100

    Nuovi es

--- Es/Lezioni/sommaRic.c
+++ Es/Lezioni/sommaRic.c
@@ -2,28 +2,34 @@
 #include <stddef.h>
 #define N 5
 
-
 //  prototipi
 int somma(const size_t aLen, const int a[]);
-int sommaR(const size_t aLen,const int a[], size_t i);
+int sommaR(const size_t aLen, const int a[], size_t i);
 
-int main(void){
-    int arr[N] = {1,4,6,15,19};
+int main(void)
+{
+    int arr[N] = {1, 4, 6, 15, 19};
     int som = 0;
     som = somma(N, arr);
     scanf("la somma degli el. dell'array e': %d \n", som);
     return 0;
 }
 
-// assicurasi che partiamo da zero
-int somma(const size_t aLen, const int a[]){
+// interfaccia con involucro
+// wrapper -> oscura i dettagli al main
+int somma(const size_t aLen, const int a[])
+{
     return sommaR(aLen, a, 0);
 }
 
-int sommaR(const size_t aLen,const int a[], size_t i){
-    if(i == aLen){
+int sommaR(const size_t aLen, const int a[], size_t i)
+{
+    if (i == aLen)
+    {
         return 0;
-    }else{
+    }
+    else
+    {
         return a[i] + sommaR(aLen, a, i + 1);
     }
 }
