46b6bfea382be95d72dfcef1898345613b353f13
alePrandi <ale.prandi06@gmail.com>
2025-11-24 16:05:11 +0100

    Ricorsione

--- Es/Lezioni/sommaRic.c
+++ Es/Lezioni/sommaRic.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <stddef.h>
+#include <limits.h>
 #define N 5
 
 //  prototipi
@@ -12,6 +12,9 @@ int main(void)
     int som = 0;
     som = somma(N, arr);
     scanf("la somma degli el. dell'array e': %d \n", som);
+    stampaCre(N, arr, 0);
+    puts("");
+    stampaDec(N, arr, 0);
     return 0;
 }
 
@@ -31,5 +34,60 @@ int sommaR(const size_t aLen, const int a[], size_t i)
     else
     {
         return a[i] + sommaR(aLen, a, i + 1);
+    }
+}
+
+/*
+void stampaCre(const size_t aLen, const int a[], size_t i){
+    if (i < aLen){
+        printf("%d ", a[i]);
+        stampaCre(aLen, a, i + 1);
+    }
+}
+*/
+
+void stampaCre(const size_t aLen, const int a[], size_t i){
+    if (i == aLen){
+        return;
+    }else{
+        printf("%d ", a[i]);
+        stampaCre(aLen, a, i + 1);
+    }
+}
+
+/*
+void stampaDec(const size_t aLen, const int a[], size_t i){
+    if(aLen - i -1 >= 0){
+        printf("%d ", a[aLen - i - 1]);
+        stampaDec(aLen, a, i + 1);
+    }
+}
+*/
+
+void stampaDec(const size_t aLen, const int a[], size_t i){
+    if(i == aLen){
+        return;
+    }else{
+        // inverto stampa e chiamata per stampare prima i successivi
+        stampaDec(aLen, a, i + 1);
+        printf("%d ", a[i]);
+    }
+}
+
+int min(const size_t aLen, const size_t a[], size_t i){
+    if(i == aLen){
+        return INT_MAX;
+    }else{
+        int minCoda = min(aLen, a, i +1);
+        return(a[i] < minCoda) ? a[i] : minCoda;
+    }
+}
+
+int max(const size_t aLen, const size_t a[], size_t i){
+    if(i == aLen){
+        return INT_MIN;
+    }else{
+        int maxCoda = max(aLen, a, i +1);
+        return(a[i] > maxCoda) ? a[i] : maxCoda;
     }
 }
